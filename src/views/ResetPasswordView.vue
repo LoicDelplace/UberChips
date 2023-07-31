bdde7c7aa8a0cee507d1fa4841382dadef2c9823
Loic Delplace <loic.delplace59@gmail.com>
2023-07-31 13:03:30 +0200

    📄 Add new view ResetPassword

---
+++ src/views/ResetPasswordView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Forgot Password ?</h2>
+    <p>This view not working at the moment. I need to learn more back-end 🥸</p>
+    <p>But you can still try to reset your password by clicking on the button below</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+p {
+    margin: 2em;
+}
+</style>
